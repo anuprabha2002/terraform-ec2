7823504e5d6f80a1c6fabf87a507498089bff0ed
Anuprabha <anuprabha.mv@guhatek.com>
2026-05-26 15:15:36 +0530

    Add Terraform module and remote backend configuration

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-anu02-dev"
+    key            = "dev/terraform.tfstate"
+    region         = "eu-north-1"
+    dynamodb_table = "terraform-locks"
+    profile        = "personal"
+  }
+}
